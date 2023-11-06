6e63cb6277e9343613f112520192eb60d856db78
drnens <117173223+drnens@users.noreply.github.com>
2023-11-06 07:22:39 +0300

    Update clock_enes.py

--- clock_enes.py
+++ clock_enes.py
@@ -18,7 +18,7 @@ def get_random_quote():
 def update_quote():
     quote = get_random_quote()
     quote_label.config(text=quote)
-    quote_label.after(20*60*500, update_quote)
+    quote_label.after(20*60*500, update_quote) # Duration time is about 20 mins.
 
 def update_time():
     current_time = strftime('%H:%M:%S')
@@ -28,25 +28,19 @@ def update_time():
 # Creating the main window
 root = tk.Tk()
 root.title("Focus Clock")
-
-# Set the background color of the window to black
 root.configure(bg='black')
 
-# Setting up the label widget to display the time
-time_label = tk.Label(root, font=('calibri', 100, 'bold'), background='black', foreground='#00FF00')  # Dark green color
+time_label = tk.Label(root, 
+                      font=('calibri', 100, 'bold'), 
+                      background='black', 
+                      foreground='#00FF00')  # Dark green color
 time_label.pack(anchor='center')
 
-# Setting up the label widget to display random quotes
 quote_label = tk.Label(root, font=('calibri', 14, 'italic'), background='black', foreground='white', wraplength=400)
 quote_label.pack(anchor='s', pady=20)  # Place it below the time_label
 
-# Set a fixed window size
 root.geometry("600x370")  # Adjust the size as needed
-
-# Make the window non-resizable
 root.resizable(False, False)
-
-# Make the window always stay on top
 root.attributes("-topmost", True)
 
 update_time()  # Call the update_time function initially to display the current time
